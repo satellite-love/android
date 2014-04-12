cc322b71bdabc6c78a603e41841f615eef8026aa
operando <okanoshinobu8@gmail.com>
2014-04-12 15:05:20 +0900

    Android project template

---
+++ src/jp/co/satellitelove/app/MainActivity.java
@@ -0,0 +1,15 @@
+package jp.co.satellitelove.app;
+
+import jp.co.satellitelove.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+}
