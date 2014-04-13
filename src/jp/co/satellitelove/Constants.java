c40ecf1831ebcba3974ee436e801184ca79455b3
operando <okanoshinobu8@gmail.com>
2014-04-13 11:39:43 +0900

    定数クラス追加

---
+++ src/jp/co/satellitelove/Constants.java
@@ -0,0 +1,11 @@
+package jp.co.satellitelove;
+
+public class Constants {
+
+	public static String ACTION_TASK_END = "task_end";
+
+	public static String INTENT_KEY_TASK_ID = "_id";
+
+	public static String INTENT_KEY_TASK = "task";
+
+}
