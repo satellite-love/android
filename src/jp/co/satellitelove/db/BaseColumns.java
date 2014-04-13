0bfe3924b9af704926b00c8eea8eb31fb0d2e3d9
operando <okanoshinobu8@gmail.com>
2014-04-13 11:40:43 +0900

    ローカルBD用定数クラス追加

---
+++ src/jp/co/satellitelove/db/BaseColumns.java
@@ -0,0 +1,13 @@
+package jp.co.satellitelove.db;
+
+public interface BaseColumns {
+
+	String _ID = "_id";
+
+	String _DATE_ADDED = "_date_added";
+
+	String _DATE_MODIFIED = "_date_modified";
+
+	String _DELETE_FLAG = "_delete_flag";
+
+}
---
+++ src/jp/co/satellitelove/db/DbConstants.java
@@ -0,0 +1,13 @@
+package jp.co.satellitelove.db;
+
+public class DbConstants implements BaseColumns {
+
+	public static final String TABLE_NAME_TASKS = "tasks";
+
+	// -----------------------------Tasks-------------------------------
+	public static final String COLUMN_TASKS_TASK_CONTENT = "task_content";
+	public static final String COLUMN_TASKS_SATELLITE_NAME = "satellite_name";
+	public static final String COLUMN_TASKS_NOTICE_TIME = "notice_time";
+	public static final String COLUMN_TASKS_NOTICE_FLAG = "notice_flag";
+
+}
